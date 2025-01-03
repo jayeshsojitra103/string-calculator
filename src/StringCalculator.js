8371949e95939d6dcde95bbefc2a9ca1ee9dfc08
jayeshsojitra103 <jayesh.sojitra103@gmail.com>
2025-01-03 14:11:24 +0530

    Step 2: Handling One and Two Numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,4 +1,7 @@
 export const add = (numbers) => {
   if (!numbers) return 0;
-  return 0;
+
+  const nums = numbers.split(",").map(Number);
+
+  return nums.reduce((sum, num) => sum + num, 0);
 };
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,11 @@ import { add } from "./StringCalculator";
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number for a single input", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns the sum for two numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
