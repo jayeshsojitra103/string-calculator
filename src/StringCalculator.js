a8b8d58dbc6ba8bdbb4bdbd9d4c8f3ca5a5c7565
jayeshsojitra103 <jayesh.sojitra103@gmail.com>
2025-01-03 14:41:17 +0530

    Step 4: Supporting Different Delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,16 @@
 export const add = (numbers) => {
   if (!numbers) return 0;
 
-  const nums = numbers.replace(/\n/g, ",").split(",").map(Number);
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    console.log("delimiter:::::>", delimiter);
+    delimiter = parts[0].slice(2);
+    console.log("delimiter:::::>", delimiter);
+    numbers = parts[1];
+  }
+
+  const nums = numbers.split(new RegExp(`[${delimiter}\n]`)).map(Number);
 
   return nums.reduce((sum, num) => sum + num, 0);
 };
