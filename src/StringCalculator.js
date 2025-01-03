2f1313d3ad7b7ba340fe21943bdfe3f54a1b7852
jayeshsojitra103 <jayesh.sojitra103@gmail.com>
2025-01-03 14:24:08 +0530

    Step 3: Handling New Lines

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,7 @@
 export const add = (numbers) => {
   if (!numbers) return 0;
 
-  const nums = numbers.split(",").map(Number);
+  const nums = numbers.replace(/\n/g, ",").split(",").map(Number);
 
   return nums.reduce((sum, num) => sum + num, 0);
 };
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -11,3 +11,7 @@ test("returns the number for a single input", () => {
 test("returns the sum for two numbers", () => {
   expect(add("1,5")).toBe(6);
 });
+
+test("handles new lines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
